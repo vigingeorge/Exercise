1d4f953a8c6b3a27dd21d2d95c90648e43513a07
Vigin George <vigin.george@if.no>
2023-09-15 16:41:45 +0200

    added comments

--- Task 1/consumer_api/app/main.py
+++ Task 1/consumer_api/app/main.py
@@ -7,22 +7,27 @@ from fastapi import FastAPI
 from pydantic import BaseModel, StrictStr
 from os import environ as env
 
-
-
+# Create a FastAPI app instance
 app = FastAPI()
 
-KAFKA_INSTANCE = env.get("KAFKA_INSTANCE","localhost:9092")
+# Get the Kafka instance address from environment variables or use a default value
+KAFKA_INSTANCE = env.get("KAFKA_INSTANCE", "localhost:9092")
 
-loop = asyncio.get_event_loop()
-
+# Get the topic name from environment variables
 topic = env.get("topic")
 
+# Get the asyncio event loop
+loop = asyncio.get_event_loop()
+
+# Create an AIOKafkaConsumer instance for consuming messages from the specified topic
 consumer = AIOKafkaConsumer(topic, bootstrap_servers=KAFKA_INSTANCE, loop=loop)
 
+# Define an asynchronous function to consume messages from Kafka
 async def consume():
     await consumer.start()
     try:
         async for msg in consumer:
+            # Process the consumed message
             print(
                 "consumed: ",
                 msg.topic,
@@ -37,14 +42,16 @@ async def consume():
     finally:
         await consumer.stop()
 
-
+# Define an event handler to start message consumption when the app starts
 @app.on_event("startup")
 async def startup_event():
     try:
+        # Create a task to run the consume function asynchronously
         loop.create_task(consume())
     except errors.KafkaError as kafka_error:
         logging.error(f"Kafka error during startup: {kafka_error}")
 
+# Define an event handler to stop the Kafka consumer when the app shuts down
 @app.on_event("shutdown")
 async def shutdown_event():
     try:
@@ -52,9 +59,11 @@ async def shutdown_event():
     except errors.KafkaError as kafka_error:
         logging.error(f"Kafka error during shutdown: {kafka_error}")
 
+# Define a root endpoint for the FastAPI app
 @app.get("/consumer")
 def read_root():
-    return {f"Consuming {topic} sucessfully..check log for details"}
+    return {f"Consuming {topic} successfully. Check the log for details."}
 
+# Entry point for running the application
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
--- Task 1/producer_api/app/main.py
+++ Task 1/producer_api/app/main.py
@@ -7,20 +7,23 @@ from fastapi import FastAPI
 from pydantic import BaseModel, StrictStr
 from os import environ as env
 
-
+# Define a data model for the incoming message
 class ProducerMessage(BaseModel):
     message: StrictStr = ""
 
-
+# Create a FastAPI app instance
 app = FastAPI()
 
+# Get the Kafka instance address from environment variables or use a default value
 KAFKA_INSTANCE = env.get("KAFKA_INSTANCE", "localhost:9092")
 
+# Get the asyncio event loop
 loop = asyncio.get_event_loop()
 
+# Create an AIOKafkaProducer instance for producing messages
 aioproducer = AIOKafkaProducer(loop=loop, bootstrap_servers=KAFKA_INSTANCE)
 
-
+# Define an event handler to start the Kafka producer when the app starts
 @app.on_event("startup")
 async def startup_event():
     try:
@@ -28,7 +31,7 @@ async def startup_event():
     except errors.KafkaError as kafka_error:
         logging.error(f"Kafka error during startup: {kafka_error}")
 
-
+# Define an event handler to stop the Kafka producer when the app shuts down
 @app.on_event("shutdown")
 async def shutdown_event():
     try:
@@ -36,15 +39,16 @@ async def shutdown_event():
     except errors.KafkaError as kafka_error:
         logging.error(f"Kafka error during shutdown: {kafka_error}")
 
-
+# Define a root endpoint for the FastAPI app
 @app.get("/")
 def read_root():
     return {"Producer home"}
 
-
+# Define an endpoint for producing messages to a Kafka topic
 @app.post("/producer/{topicname}")
 async def kafka_produce(msg: ProducerMessage, topicname: str):
     try:
+        # Produce the message to the specified Kafka topic
         await aioproducer.send(topicname, json.dumps(msg.dict()).encode("ascii"))
         response = {f"{msg} posted successfully on topic {topicname}"}
         return response
@@ -52,5 +56,6 @@ async def kafka_produce(msg: ProducerMessage, topicname: str):
         logging.error(f"Kafka error while producing: {kafka_error}")
         return {"error": "Failed to produce message to Kafka"}
 
+# Entry point for running the application
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
