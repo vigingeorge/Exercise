da6a5f67d2e915a1e53fe574e1f2d8a7c4221525
Vigin George <vigin.george@if.no>
2023-09-15 20:49:43 +0200

    Update variables.tf

--- Task 2/terraform/variables.tf
+++ Task 2/terraform/variables.tf
@@ -1,40 +1,48 @@
 variable "project" {
-  default = ""
+  description = "The project name associated with the resources."
+  default     = "myproject"
 }
 
 variable "environment" {
-  default = ""
+  description = "The environment name associated with the resources."
+  default     = "dev"
 }
 
 variable "team" {
-  default = ""
+  description = "The team name associated with the resources."
+  default     = "avengers"
 }
 
 variable "instance" {
-  default = "001"
+  description = "The instance number or identifier."
+  default     = "001"
 }
 
 variable "location" {
-  default = "westeurope"
+  description = "The Azure region where resources will be deployed."
+  default     = "westeurope"
 }
 
-
 variable "common_tags" {
-  default = {
-    Environment  = "",
-    ITSystemCode = "",
-    Responsible  = ""
+  description = "Common tags to apply to Azure resources."
+  default     = {
+    Environment  = "development",
+    ITSystemCode = "batman",
+    Responsible  = "John Doe"
   }
 }
 
 variable "vnet_address_space" {
-  default = ""
+  description = "The address space for the Virtual Network (VNet)."
+  default     = "10.0.0.0/16"
 }
 
 variable "sub_address_prefix" {
-  default = ""
+  description = "The address prefix for the subnet within the VNet."
+  default     = "10.0.1.0/24"
 }
 
 variable "subnet_name" {
-  default = ""
+  description = "The name of the subnet within the VNet."
+  default     = "subnet"
 }
